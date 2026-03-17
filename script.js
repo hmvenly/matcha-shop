1f89709629b6c34d3ac9268cef23c1b9cacb7ef1
nunur <shinebuthill@gmail.com>
2026-03-17 12:29:16 +0400

    Update menu script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const openBtn = document.getElementById('openMenu');
+const closeBtn = document.getElementById('closeMenu');
+const overlay = document.getElementById('menuOverlay');
 
+openBtn.addEventListener('click', () => {
+    overlay.style.width = "100%";
+});
+
+closeBtn.addEventListener('click', () => {
+    overlay.style.width = "0%";
+});
